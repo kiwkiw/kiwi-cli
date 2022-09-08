3009b39930c7b53580a1e351eb982284774bd383
kiwi <478480767@qq.com>
2022-09-08 09:37:33 +0800

    fix

--- bin/cli.js
+++ bin/cli.js
@@ -46,7 +46,7 @@ program
     // 监听 --help 执行
     .on('--help', () => {
         // 使用 figlet 绘制 Logo
-        console.log('\r\n' + figlet.textSync('kiwi-cli', {
+        console.log('\r\n' + figlet.textSync('pengkiw-cli', {
             font: 'Ghost',
             horizontalLayout: 'default',
             verticalLayout: 'default',
@@ -54,7 +54,7 @@ program
             whitespaceBreak: true
         }));
         // 新增说明信息
-        console.log(`\r\nRun ${chalk.cyan(`kiwi-cli <command> --help`)} for detailed usage of given command\r\n`)
+        console.log(`\r\nRun ${chalk.cyan(`pengkiw-cli <command> --help`)} for detailed usage of given command\r\n`)
     })
 
 
